7289aeda1dc6da2b54034f9f76ac63a95c1b024f
ZaidKhan15 <zaid159080@gmail.com>
2021-11-16 23:08:47 +0530

    corrected some mistakes

--- DSA practice c++/BitManipulation/common_ops.cpp
+++ DSA practice c++/BitManipulation/common_ops.cpp
@@ -11,26 +11,34 @@ int getIthBit(int x,int index)
     return(x & mask)>0?1:0;
 }
 
-void setIthBit(int x,int index){
+void setIthBit(int &x,int index){
 
     int mask = (1<<index);
     x = x | mask;
 }
 
-void ClearIthBit(int x, int index){
+void ClearIthBit(int &x, int index){
  
  int mask = ~(1<<index);
  x = x & mask;
 }
 
+
+void UpdateIthBit(int &x,int index,int value)  //value =00000000 /00001000 according to x
+   {
+       ClearIthBit(x,index);
+       int mask = (value<<index);
+        x = x | mask;
+   }
+
 int main()
 {
-    int x = 5;
+    int x = 13;
     int index;
     cin>>index;
 
-    cout<<getIthBit(x,index)<<endl;
+    UpdateIthBit(x,index,1);
 
-    ClearIthBit(x,index);
+  //  ClearIthBit(x,index);
     cout<<x;
 }
